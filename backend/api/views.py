5518b824267ad1b17c74c00d11863770cfe184b3
GhazalTajik1998 <ghazal.tajik.77@gmail.com>
2023-02-14 20:30:31 +0330

    User views added

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth import get_user_model
+from rest_framework.generics import ListAPIView
+from account.models import User
+from .serializers import UserSerializer
 # Create your views here.
+
+
+class UserListView(ListAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class =  UserSerializer
+
+
+
