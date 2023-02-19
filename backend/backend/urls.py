877e9bcc492f7b402cd4e1d4921cec43f38fc517
GhazalTajik1998 <ghazal.tajik.77@gmail.com>
2023-02-19 18:29:04 +0330

    some shits added by lack knowlodge of git by me

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'), name='rest_framework'),
     path('api/', include('api.urls')),
+    path('test/', include('tests.urls')),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh')
 ]
