44c0afaaf7c35659f50bfaa63ae0f905dc86c053
GhazalTajik1998 <ghazal.tajik.77@gmail.com>
2023-02-22 18:21:01 +0330

    CRUD views added by using APIView

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserListView, EventViewSet
+from .views import UserListView, EventList, EventDetailView
 
-router = routers.SimpleRouter()
-router.register(r'events', EventViewSet)
+# router = routers.SimpleRouter()
+# router.register(r'events', EventViewSet)
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
 
-urlpatterns += [
-    path('users/', UserListView.as_view(), name="users_list")
+urlpatterns = [
+    path('users/', UserListView.as_view(), name="users_list"),
+    path('events/', EventList.as_view(), name='event_list'),
+    path('event/<int:pk>', EventDetailView.as_view(), name="detail_event")
+    # path('event/<int:pk>', EventView.as_view())
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import views, authentication, status
+from rest_framework.response import Response
 from account.models import User
 from .serializers import UserSerializer, EventSerializer
 from events.models import Event
@@ -14,10 +16,56 @@ class UserListView(ListAPIView):
     permission_classes = [IsAuthenticated]
 
 
-class EventViewSet(ModelViewSet):
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
+# Viewsets for Event
+# class EventViewSet(ModelViewSet):
+#     queryset = Event.objects.all()
+#     serializer_class = EventSerializer
 
 
 
+# Using APIView 
+class EventList(views.APIView):
+    # auhtentication_classes = [authentication.TokenAuthentication]
+    # serializer_class = EventSerializer
 
+    def get(self, request, format=None):
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EventDetailView(views.APIView):
+    def get(self, request, pk):
+        event = get_object_or_404(Event, pk=pk)
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+    
+    def delete(self, request, pk):
+        event = get_object_or_404(Event, pk=pk)
+        event.delete()
+        return Response(status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        event = get_object_or_404(Event, pk=pk)
+        serializer = EventSerializer(event, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+
+
+    
+
+# # Generic Views
+# class EventList(ListAPIView):
+#     queryset = Event.objects.all()
+#     serializer_class = EventSerializer
