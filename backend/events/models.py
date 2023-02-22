a1ae93ef1c8203bdd00eb49c1efd24d0edb082ee
GhazalTajik1998 <ghazal.tajik.77@gmail.com>
2023-02-22 10:54:51 +0330

    Event app created

---
+++ backend/events/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# # Create your models here.
+# class Event(models.Model):
+#     name = models.CharField(max_length=255, )
+#     subject = models.CharField(max_length=500)
+#     description = models.TextField()
+#     price = models.DecimalField()
+#     start_date = models.DateTimeField()
+#     end_date = models.DateTimeField()
+    
